ac21d3ab0b1fff1fc1389a7cf05becf58d355253
vyishere <67214414+vyishere@users.noreply.github.com>
2022-02-11 21:11:50 +0700

    Update mute.js
    now stable!

--- commands/mute.js
+++ commands/mute.js
@@ -2,17 +2,10 @@ const Discord = require("discord.js");
 const ms = require("ms");
 
 exports.execute = async (client, message, args) => { 
+  if (!message.member.hasPermission("MANAGE_MESSAGES")) 
+  message.channel.send("no")
+  else {
   const muterole = message.guild.roles.cache.find(r => r.name === 'Muted');
-        if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("**You don't have the permissions!**").then(msg => {
-            msg.delete({ timeout: 5000 })
-            })
-        if (!message.guild.me.hasPermission("MANAGE_MESSAGES")) return message.channel.send("**I don't have the permissions!**").then(msg => {
-            msg.delete({ timeout: 5000 })
-            })
-        if (!args[0]) return message.channel.send("**Please provide a user!**").then(msg => {
-            msg.delete({ timeout: 5000 })
-            })
-
   let muteMember = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args[0].toLocaleLowerCase()) || message.guild.members.cache.find(ro => ro.displayName.toLowerCase() === args[0].toLocaleLowerCase());
             if (!muteMember) return message.channel.send("**That user is not in this guild.**").then(msg => {
                     msg.delete({ timeout: 5000 })
@@ -22,8 +15,7 @@ exports.execute = async (client, message, args) => {
             })
 
   //start of create role          
-  if(message.member.hasPermission("MANAGE_MESSAGES")) 
- {
+
   if(!muterole){
     try {
  muterole = await message.guild.roles.create({
@@ -43,22 +35,22 @@ exports.execute = async (client, message, args) => {
   })
  });
 }
-
  catch (e) {
- console.log(e.stack);}
+ console.log(e.stack);}}
+  //end of create role
 
   let mutetime = args[1];
   if(!mutetime) return message.reply("You didn't specify a time!");
 
-  await(tomute.roles.add(muterole.id));
-  message.reply(`<@${tomute.id}> has been muted for ${ms(ms(mutetime))}`);
+  await(muteMember.roles.add(muterole.id));
+  message.reply(`<@${muteMember.id}> has been muted for ${ms(ms(mutetime))}`);
 
   setTimeout(function(){
-    tomute.roles.remove(muterole.id);
-    message.channel.send(`<@${tomute.id}> has been unmuted!`);
+    muteMember.roles.remove(muterole.id);
+    message.channel.send(`<@${muteMember.id}> has been unmuted!`);
   }, ms(mutetime));
-  }
-}}
+  }}
+
  //end of module
 
 module.exports.help = {
